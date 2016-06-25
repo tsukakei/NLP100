a78df03773a19cb6b75af65358c2fc100645ecaf
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2016-06-26 00:15:03 +0900

    Chapter1

--- Chapter1/08.py
+++ Chapter1/08.py
@@ -1,6 +1,9 @@
 def cipher(s):
-    return "".join(chr(219-ord(c)) if 'a' <= c <= 'z' else c in s)
+    return "".join(chr(219-ord(c)) if 'a' <= c <= 'z' else c for c in s)
 
 if __name__ == '__main__':
     s = "Hello, World!"
-
+    c = cipher(s)
+    print s
+    print c
+    print cipher(c)
--- Chapter1/09.py
+++ Chapter1/09.py
@@ -1,8 +1,11 @@
-import shuffle from random
+from random import shuffle
 def typoglycemia(word): 
     if len(word) < 4:
         return word
-    mid_word = word[1:-1]
+    mid_word = list(word[1:-1])
     shuffle(mid_word)
-    return word[0] + mid_word + word[-1]
+    return word[0] + "".join(mid_word) + word[-1]
 
+if __name__ == '__main__':
+    s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+    print " ".join(typoglycemia(w.strip(".,")) for w in s.split())
