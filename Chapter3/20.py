2e3af0d65b3b7849e10e6824daefc7696decadb5
Keita Tsukamoto <t.k.columbus1012@gmail.com>
2016-09-16 22:25:54 +0900

    20を解答

--- Chapter3/20.py
+++ Chapter3/20.py
@@ -1,10 +1,12 @@
 # coding: UTF-8
 import json
+import codecs
 with open("jawiki-country.json", "r") as wiki, open("jawiki-uk.txt", "w") as eng:
     country = wiki.readline()
     while country:
         content = json.loads(country)
         if content["title"] == u"イギリス":
+            eng = codecs.lookup('utf-8')[-1](eng)
             eng.write(content["text"])
             break
         country = wiki.readline()
